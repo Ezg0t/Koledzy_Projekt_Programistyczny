0992c418eddd00167ff41b0403daaef69168fa03
Krystian Klaczyński <66339829+Ezg0t@users.noreply.github.com>
2022-05-31 15:57:54 +0200

    zmiana

--- zegarki_scraper/zegarki_scraper/start.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-class odpal():
-    def scrapy_to_db():
-        process = CrawlerProcess(get_project_settings())
-        process.crawl('zegarownia')
-        process.start()
-odpal.scrapy_to_db()
